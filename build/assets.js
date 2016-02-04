e3b8a58e5397a52e0fe7d9b56756d3137ebd4fec
Derek Rush <drush81@gmail.com>
2016-02-04 05:47:51 -0800

    main push

--- build/assets.js
+++ build/assets.js
@@ -3,13 +3,19 @@ var gulp = require('gulp'),
     imagemin = require('gulp-imagemin'),
     notify = require('gulp-notify'),
     sass = require('gulp-sass'),
+    bower = require('gulp-bower')
     jade = require('gulp-jade'),
     paths = require('./paths'),
     changed = require('gulp-changed'),
     plumber = require('gulp-plumber'),
     sourcemaps = require('gulp-sourcemaps'),
     del = require('del'),
-    assign = Object.assign || require('object.assign');
+    assign = Object.assign || require('object.assign')
+
+// gulp.task('icons', function() { 
+//   return gulp.src(paths.font_awesome)
+//     .pipe(gulp.dest(paths.fonts))
+// })
 
 gulp.task('clean', function() {
   return gulp.src('/dist/*')
@@ -25,12 +31,12 @@ gulp.task('images', function() {
 })
 
 gulp.task('styles', function() {
-  gulp.src('./src/stylesheets/*.scss')
+  gulp.src(paths.scss)
   .pipe(sass({
     includePaths: require('node-neat').includePaths,
     errLogToConsole: true
   }))
-  .pipe(gulp.dest('./dist/css/'))
+  .pipe(gulp.dest(paths.css))
 })
 
 gulp.task('templates', function() {
@@ -41,9 +47,9 @@ gulp.task('templates', function() {
 
 gulp.task('es2js', function () {
   return gulp.src(paths.es2015)
-    .pipe(plumber())
-    .pipe(changed(paths.output, {extension: '.js'}))
-    .pipe(sourcemaps.init({loadMaps: true}))
-    .pipe(sourcemaps.write({includeContent: false, sourceRoot: paths.sourceMapRelativePath }))
+    // .pipe(plumber())
+    // .pipe(changed(paths.output, {extension: '.js'}))
+    // .pipe(sourcemaps.init({loadMaps: true}))
+    // .pipe(sourcemaps.write({includeContent: false, sourceRoot: paths.sourceMapRelativePath }))
     .pipe(gulp.dest(paths.js));
 })
--- build/paths.js
+++ build/paths.js
@@ -1,12 +1,15 @@
 var path = require('path'),
     appRoot = './src/',
+    bowerRoot = './bower_components/',
     outputRoot = './dist/'
 
 module.exports = {
-  root:   appRoot,
-  output: outputRoot,
-  es2015:    appRoot + 'js/*.js',
-  js:     outputRoot,
+  bower_css: bowerRoot + 'css/',
+  bower_js: bowerRoot + 'js/',
+  font_awesome: bowerRoot + 'dist/fonts/**.*',
+  fonts: outputRoot + 'fonts/',
+  es2015:    appRoot + 'js/*.{js, min.js}',
+  js:     outputRoot + 'js/',
   jade:   appRoot + 'views/*.{jade, html}',
   html:   outputRoot,
   scss:   appRoot + 'stylesheets/*.{scss, sass, css}',
