6e86c42a1ec7666d257bea9fdd655d19086daa21
Derek Rush <drush81@gmail.com>
2016-02-08 01:15:49 -0800

    main push

--- dist/js/default.js
+++ dist/js/default.js
@@ -1,19 +1,26 @@
 $(() => {
-    $('header').parallax({
-      imageSrc: '../images/resume_bg.jpg',
-      positionY: '-80px'
-    })
+  $('header').parallax({
+    imageSrc: '../images/resume_bg.jpg',
+    positionY: '-80px'
+  })
 
-    $('#contact').parallax({
-      imageSrc: '../images/resume_bg.jpg',
-      positionY: '-700px'
-    })
+  $('#contact').parallax({
+    imageSrc: '../images/resume_bg.jpg',
+    positionY: '-700px'
+  })
 
-    $(document).scroll((e) => {
-      // if( $(this).scrollTop() > 300 ) {
-      //     mn.addClass(mns);
-      //   } else {
-      //     mn.removeClass(mns);
-      //   }
-    })
+  var tools_pos = $('#tools').position()
+
+  // $(document).scroll((e) => {
+  //   console.log("hgj", e);
+  //   if(!$("#tools").is(":hidden")){
+  //     if($(this).scrollTop() > (tools_pos.top - 40)){
+  //       console.log("uygjg");
+  //       $('#tools').animate({height: "20px"}, 500)
+  //     }
+  //   }
+    // a = $(e.target).offset() + $(e.target).height();
+    // b = $('#tools').scrollTop() + $('header').height();
+    // if (a < b) $().fadeTo(500,1);
+  // })
 })
