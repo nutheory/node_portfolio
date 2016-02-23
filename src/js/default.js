a1b5bac80913d105f9c767fcf5dc02c6429b36f2
Derek Rush <drush81@gmail.com>
2016-02-23 05:06:25 -0800

    mostly text

--- src/js/default.js
+++ src/js/default.js
@@ -9,18 +9,5 @@ $(() => {
     positionY: '-700px'
   })
 
-  var tools_pos = $('#tools').position()
 
-  // $(document).scroll((e) => {
-  //   console.log("hgj", e);
-  //   if(!$("#tools").is(":hidden")){
-  //     if($(this).scrollTop() > (tools_pos.top - 40)){
-  //       console.log("uygjg");
-  //       $('#tools').animate({height: "20px"}, 500)
-  //     }
-  //   }
-    // a = $(e.target).offset() + $(e.target).height();
-    // b = $('#tools').scrollTop() + $('header').height();
-    // if (a < b) $().fadeTo(500,1);
-  // })
 })
